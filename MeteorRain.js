f508fa94f38c366ee1023ea82299e50f1900f6d8
JasmineGreenTea <clorisdong2001@gmail.com>
2022-02-20 19:40:00 -0600

    Submission

--- MeteorRain.js
+++ MeteorRain.js
@@ -12,15 +12,15 @@ BgBlue = '\x1b[44m'
 const RESET = '\x1b[0m'
 const w = process.stdout.columns
 const char =['★', '☆', '✰', '✧', '✦', '⋱', '⋰']
-const eff = ['\x1b[5m', '\x1b[2m', '\x1b[4m']
+const eff = ['\x1b[5m', '\x1b[2m', '\x1b[7m']
 
 function draw () {
   setTimeout(draw, 1000/12)
-  let output = '\x1b[0m\x1b[44m\x1b[93m'
+  let output = '\x1b[0m'
   for (let i = 0; i < w; i++) {
     let r = Math.floor(Math.random() * char.length)
     let g = Math.floor(Math.random() * eff.length)
-    output += eff[g] + char[r]
+    output += '\x1b[0m\x1b[36m' + eff[g] + char[r]
   }
   console.log(output)
 }
