9406010a419573612e1d693afdcb1d60ba8c7a2d
JasmineGreenTea <clorisdong2001@gmail.com>
2022-02-21 11:06:40 -0600

    draft updated

--- Exper.js
+++ Exper.js
@@ -1,38 +1,17 @@
 /*
   Try to print out characters one after another with a delay in-b/w
 */
-// const RESET = '\x1b[0m'
-// const w = process.stdout.columns
-// const char =['★', '☆', '✰', '✧', '✦', '⋱', '⋰']
-// const eff = ['\x1b[5m', '\x1b[2m', '\x1b[7m']
-//
-// function draw () {
-//   setTimeout(draw, 1000/12)
-//   let output = '\x1b[0m'
-//   for (let i = 0; i < w; i++) {
-//     let r = Math.floor(Math.random() * char.length)
-//     let g = Math.floor(Math.random() * eff.length)
-//     output += '\x1b[0m\x1b[36m' + eff[g] + char[r]
-//   }
-//   console.log(output)
-// }
-
-// draw()
-
 const RESET = '\x1b[0m'
 const w = process.stdout.columns
-const char =['★', '☆', '✰', '✧', '✦', '⋱', '⋰']
-const eff = ['\x1b[5m', '\x1b[2m', '\x1b[7m']
+const char =['a', 'b', 'c', 's', 'f', 'g', 'h']
 
-let output = ""
+let s = ""
+
 function draw () {
-  let output = '\x1b[0m'
-  for (let i = 0; i < w; i++) {
-    let r = Math.floor(Math.random() * char.length)
-    let g = Math.floor(Math.random() * eff.length)
-    output = '\x1b[0m\x1b[36m' + eff[g] + char[r]
-  }
-process.stdout.write(output)
+  setTimeout(draw, 1000/30)
+  let r = Math.floor(Math.random() * char.length)
+  s = char[r]
+  process.stdout.write(s)
 }
 
 draw()
